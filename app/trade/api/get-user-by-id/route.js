42ed90a822bbef15f14ae58b29f1b78ae5016e6f
lidor4x4 <lidor4x4@gmail.com>
2023-06-22 14:18:14 +0300

    ts is not cool

--- app/trade/api/get-user-by-id/route.js
+++ app/trade/api/get-user-by-id/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
-import { Clerk, User } from '@clerk/clerk-sdk-node';
+import { Clerk } from '@clerk/clerk-sdk-node';
 
-export async function GET(request: Request): Promise<NextResponse> {
+export async function GET(request) {
     try {
         const url = new URL(request.url); // Create a new URL object from the request URL
         const searchParams = url.searchParams; // Get the search params from the URL object
@@ -9,14 +9,14 @@ export async function GET(request: Request): Promise<NextResponse> {
         const token = searchParams.get('token'); // Get the value of the token parameter
 
         const clerk = new Clerk({
-            secretKey: process.env.CLERK_SECRET_KEY!,
+            secretKey: process.env.CLERK_SECRET_KEY,
             jwtKey: `Bearer ${token}`,
         });
 
-        const user: User = await clerk.users.getUser(userId);
+        const user = await clerk.users.getUser(userId);
 
         return NextResponse.json({ user });
-    } catch (error: any) {
+    } catch (error) {
         console.error('Error fetching user:', error);
         return NextResponse.json({ error: 'Failed to fetch user' }, { status: 500 });
     }
